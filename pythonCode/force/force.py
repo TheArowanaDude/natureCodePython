72b17adda5ca0d431ad798fdd2fab2cdb1504fac
TheArowanaDude <micropachycephalosaurs@gmail.com>
2020-08-21 12:27:06 -0500

    update function

--- pythonCode/force/force.py
+++ pythonCode/force/force.py
@@ -1,1 +1,14 @@
-print("May the force be with you!")
+import numpy as np
+
+class Force: 
+    accelerationVector = np.array([0.00,0.00])
+    locationVector = np.array([0.00,0.00])
+    velocityVector = np.array([0.00,0.00])
+
+    def update(self): 
+        self.velocityVector += self.accelerationVector
+        self.locationVector += self.velocityVector
+        self.accelerationVector *= 0
+
+
+
