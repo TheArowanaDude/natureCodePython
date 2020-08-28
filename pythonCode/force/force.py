88fedc1fd2c6bdd8672f1650ff84a53f957bfae8
TheArowanaDude <micropachycephalosaurs@gmail.com>
2020-08-27 19:19:54 -0500

    added bounce function so animation stays within display

--- pythonCode/force/force.py
+++ pythonCode/force/force.py
@@ -4,7 +4,7 @@ import turtle
 
 class Force: 
     accelerationVector = np.array([0.00,0.00])
-    locationVector = np.array([0.00,0.00])
+    locationVector = np.array([150.00,150.00])
     velocityVector = np.array([0.00,0.00])
 
     def setupBoard(self, width = 500, height = 500): 
@@ -16,14 +16,24 @@ class Force:
         self.screen.setup(width, height)
         self.screen.tracer(0)
         self.t.speed(0)
-        self.screen.setworldcoordinates(0,0,width,height)
+        #self.screen.setworldcoordinates(0,0,width,height)
+
+    def bounce(self): 
+        if self.locationVector[0] > self.screen.screensize()[0] or self.locationVector[0] < 0: 
+            print("width   " + str(self.screen.screensize()[0]))
+            self.velocityVector[0] *= -1
+
+        if self.locationVector[1] > self.screen.screensize()[1] or self.locationVector[1] < 0: 
+            print("height   " + str(self.screen.screensize()[0]))
+            self.velocityVector[1] *= -1     
+        
 
     def drawCircle(self): 
         self.t.clear()        
         self.t.goto(self.locationVector[0], self.locationVector[1])
         self.t.dot(30)
         self.screen.update()
-        self.update
+        #self.update
 
     def applyForce(self,forceVector): 
         self.accelerationVector += forceVector; 
@@ -42,11 +52,14 @@ class Force:
 
 def main(): 
     force = Force()
-    force.setupBoard()
+    force.setupBoard(800, 800)
 
     while True: 
-        windVector = np.array([0.001,0.001])
+        windVector = np.array([0.0001,0.00])
+        gravityVector = np.array([0.000,-0.0009])
         force.applyForce(windVector)
+        force.applyForce(gravityVector)
+        force.bounce()
         force.drawCircle()
         force.update()
     
